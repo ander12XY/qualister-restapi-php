9f649fc8efa21e53e16b3be5cbe705279379a024
Júlio de Lima <info@juliodelima.com.br>
2015-04-06 12:02:07 -0300

    Versão 1.0
    API usada em cursos de teste de software promovidos pela Qualister

---
+++ home.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>API de Exemplo da Qualister</title>
+		<style>
+			body {
+				padding: 50px;
+				text-align: center;
+				color: #fff;
+				background: #555;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>API de Exemplo da Qualister</h1>
+		<p>www.qualister.com.br</p>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,114 @@
+<?php
+
+session_start();
+
+require 'lib/Slim/Slim.php';
+require 'lib/Slim/Middleware/HttpBasicAuth.php';
+
+$app = new Slim();
+$app->add(new HttpBasicAuth('qualister', '123'));
+
+$app->get('/', 'home');
+
+function home(){
+    include "home.php";
+}
+
+$app->get('/login', 'dologin');
+
+function dologin(){
+    global $app;
+
+    $usuariologin = $app->request()->get("usuariologin");
+    $usuariosenha = $app->request()->get("usuariosenha");
+
+    $token = md5(date("%Y%m%d"));
+
+    if ($usuariologin == "aluno" && $usuariosenha == "qualister"){
+        $usuario = array(
+            "usuarioid" => 1,
+            "usuariologin" => "aluno",
+            "usuariosenha" => "qualister",
+            "usuarionome" => "Aluno Qualister",
+            "datacadastro" => date("%Y%m%d"),
+            "token" => $token
+        );
+        $status = "sucesso";
+        $mensagem = "sucesso ao fazer login";
+    } else {
+        $usuario = array();
+        $status = "erro";
+        $mensagem = "erro ao fazer login";
+    }
+
+
+    $_SESSION['token'] = $token;
+
+    header("Content-Type: application/json");
+    echo json_encode(array(
+        "status" => $status,
+        "mensagem" => $mensagem,
+        "dados" => $usuario
+    ));
+    exit();
+}
+
+$app->post('/addproduto', 'addproduto');
+
+function addproduto(){
+    global $app;
+
+    $produtonome  = $app->request()->post("produtonome");
+    $produtovalor = $app->request()->post("produtovalor");
+    $produtoestoque = $app->request()->post("produtoestoque");
+    $token = $app->request()->get("token");
+
+    if ($_SESSION['token'] == $token){
+        $produto = array(
+            "produtoid" => 1,
+            "produtonome" => $usuariologin,
+            "produtovalor" => $usuariosenha,
+            "produtoestoque" => "Aluno Qualister"
+        );
+        $status = "sucesso";
+        $mensagem = "sucesso ao adicionar o produto";
+    } else {
+        $produto = array();
+        $status = "erro";
+        $mensagem = "erro ao adicionar o produto";
+    }
+
+    $_SESSION['produto'] = $produto;
+
+    header("Content-Type: application/json");
+    echo json_encode(array(
+        "status" => $status,
+        "mensagem" => $mensagem,
+        "dados" => $produto
+    ));
+    exit();
+}
+
+$app->get('/ultimoproduto', 'ultimoproduto');
+
+function ultimoproduto(){
+    $produto = $_SESSION['produto'];
+    
+    if (count($produto) > 0){
+        $status = "sucesso";
+        $mensagem = "sucesso ao adicionar o produto";
+    } else {
+        $status = "erro";
+        $mensagem = "nenhum produto cadastrado";
+    }
+
+    header("Content-Type: application/json");
+    echo json_encode(array(
+        "status" => $status,
+        "mensagem" => $mensagem,
+        "dados" => $produto
+    ));
+    exit();
+}
+
+$app->run();
